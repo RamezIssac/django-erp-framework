f7a073d48d48c0fc2102ee05f9c6dfafa5d967ba
Ramez Issac <ramez@rasystems.io>
2020-04-01 11:24:06 +0200

    Fix in wsgi template

--- ra/project_template/project_template/project_name/urls.py
+++ ra/project_template/project_template/project_name/urls.py
@@ -1,4 +1,4 @@
-"""ra_erp_cash_proj URL Configuration
+"""{{ project_name }} URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
--- ra/project_template/project_template/project_name/wsgi.py
+++ ra/project_template/project_template/project_name/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ra_erp_cash_proj project.
+WSGI config for {{ project_name }} project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ra_erp_cash_proj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{{ project_name }}.settings')
 
 application = get_wsgi_application()
