20bdef71dd5eaf2940b30dd0bb5a00b2953533b4
Ramez Ashraf <ramez@rasystems.io>
2023-07-06 11:27:05 +0200

    Remove js files, Enhancements

--- erp_framework/reporting/views.py
+++ erp_framework/reporting/views.py
@@ -38,7 +38,8 @@ class ReportView(ReportViewBase):
         Returns the base template for the report.
         :return: a string representing the base template path (e.g. 'admin/base.html')
         """
-        return app_settings.get_admin_base_template(self.request.current_app)
+        current_app = getattr(self.request, "current_app", "")
+        return app_settings.get_admin_base_template(current_app)
 
     def get_context_data(self, **kwargs):
         from erp_framework.sites import erp_admin_site
